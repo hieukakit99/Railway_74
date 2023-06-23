4c62c3ca3a05791e040b561ca42746ecdf6f7f5c
hieukakit99 <hieukakit99@gmail.com>
2023-06-23 20:57:43 +0900

    assignment.ex2

---
+++ Java/as2/src/as2/ex2.java
@@ -0,0 +1,29 @@
+package as2;
+
+
+
+
+public class ex2 {
+	
+public static void main(String[] args) {
+System.out.println("question 1");
+	int i = 5;
+	System.out.printf("%d", i );
+// question 2 
+	System.out.println("question 2");
+	int i2 = 100000000;
+	System.out.printf ("%,d%n",i2);
+// question 3
+	System.out.println("question 3");
+	float f = 5.567098f;
+	System.out.printf("%.4f%n", f);
+// question 4
+	System.out.println(" quesiton 4");
+	String s = "Nguyen Van A";
+	System.out.print("ho va ten : " + s + " Toi dang doc than");
+// question 5
+	System.out.println("question 5");
+
+
+}
+}
---
+++ Java/as2/src/as2/ex5.java
@@ -0,0 +1,20 @@
+package as2;
+
+import java.util.Scanner;
+
+public class ex5 {
+public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Mời bạn nhập vào 3 số nguyên");
+		System.out.println("Nhập vào số 1: ");
+		int a = sc.nextInt();
+		System.out.println("Nhập vào số 2: ");
+		int b = sc.nextInt();
+		System.out.println("Nhập vào số 3: ");
+		int c = sc.nextInt();
+		System.out.println("Bạn vừa nhập vào các số: " + a + " " + b + " " + c );
+		
+	
+}
+}
